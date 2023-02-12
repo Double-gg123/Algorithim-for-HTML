18e921eb4f372bd5855f88660c7cfea76e48843d
Goodness GIBZ <71548390+Double-gg123@users.noreply.github.com>
2023-02-12 20:35:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Open a text editor and create a new document.");
+console.log("Begin the document with the opening <html> tag.");
+console.log("Add the <head> section, which includes the title of the document and any meta information.");
+console.log("Add the <body> section, which contains the content of the document.");
+console.log("Use various HTML tags, such as <p>, <h1>, <a>, etc. to format and structure the content.");
+console.log("Close the <body> and <html> tags at the end of the document.Save the document with a .html file extension.");
+console.log("Open the document in a web browser to view the final HTML document.");
